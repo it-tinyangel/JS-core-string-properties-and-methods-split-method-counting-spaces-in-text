3341922f73bad72db44d78156819bf0390c32e67
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-10-01 12:29:54 +0300

    Counting the spaces in the text
    Added JavaScript regular expression use RegExp literal (line 4)

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let userInputText = prompt(`Input your text:`);
 
 let numberOfSpaces = userInputText.split(' ').length - 1;
+// let numberOfSpaces = userInputText.split(/\s+/).length - 1;
 
 console.log(`The number of spaces in the inputted text: ${numberOfSpaces}`);
